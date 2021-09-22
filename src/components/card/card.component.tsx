779c74f7f62ae7f29f5d6130b97bea1e153698b1
Kseniya Rudenko <rudenkokseniya91@gmail.com>
2021-09-22 19:57:51 +1200

    add show and hide details button to card component

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { Horse } from "../list/list.component";
 import { Icon } from "./card.styled";
 
@@ -12,6 +13,7 @@ interface Props {
 export const BasicCard: React.FC<Props> = ({ horse }) => {
   const { name, profile } = horse;
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const [toggled, setToggled] = React.useState(false);
 
   return (
     <Card sx={{ minWidth: 275, flexDirection: "row", display: "flex" }}>
@@ -20,15 +22,26 @@ export const BasicCard: React.FC<Props> = ({ horse }) => {
         <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
           {name}
         </Typography>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary">
-          favourite food: {profile.favouriteFood}
-        </Typography>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary">
-          weight: {profile.physical.weight}
-        </Typography>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary">
-          height: {profile.physical.height}
-        </Typography>
+        {toggled && (
+          <>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+              favourite food: {profile.favouriteFood}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+              weight: {profile.physical.weight}
+            </Typography>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+              height: {profile.physical.height}
+            </Typography>
+          </>
+        )}
+        <Button
+          sx={{ marginTop: "10px" }}
+          variant="text"
+          onClick={() => setToggled(!toggled)}
+        >
+          {toggled ? "Hide details" : "Show details"}
+        </Button>
       </CardContent>
     </Card>
   );
