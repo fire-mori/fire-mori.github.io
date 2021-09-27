bcc0cb7aa91e9db3d0894d11cec4fc23f6d7dcb9
Kseniya Rudenko <rudenkokseniya91@gmail.com>
2021-09-27 20:45:57 +1300

    fix home page import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage.component';
+import HomePage from './pages/homepage/HomePage.component';
 
 function App() {
   return (
